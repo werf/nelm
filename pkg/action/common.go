77e91583e28efa41642200d359f1855af629cade
Ilya Lesikov <ilya@lesikov.com>
2024-06-27 15:06:42 +0300

    feat: add Uninstall() action
    Signed-off-by: Ilya Lesikov <ilya@lesikov.com>

--- pkg/action/common.go
+++ pkg/action/common.go
@@ -1,5 +1,18 @@
 package action
 
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io/ioutil"
+
+	"k8s.io/klog"
+	klog_v2 "k8s.io/klog/v2"
+
+	"github.com/werf/kubedog/pkg/display"
+	"github.com/werf/logboek"
+)
+
 type LogColorMode string
 
 const (
@@ -19,3 +32,70 @@ const (
 	ReleaseStorageDriverMemory     ReleaseStorageDriver = "memory"
 	ReleaseStorageDriverSQL        ReleaseStorageDriver = "sql"
 )
+
+func initKubedog(ctx context.Context) error {
+	flag.CommandLine.Parse([]string{})
+
+	display.SetOut(logboek.Context(ctx).OutStream())
+	display.SetErr(logboek.Context(ctx).ErrStream())
+
+	if err := silenceKlog(ctx); err != nil {
+		return fmt.Errorf("silence klog: %w", err)
+	}
+
+	if err := silenceKlogV2(ctx); err != nil {
+		return fmt.Errorf("silence klog v2: %w", err)
+	}
+
+	return nil
+}
+
+func silenceKlog(ctx context.Context) error {
+	fs := flag.NewFlagSet("klog", flag.PanicOnError)
+	klog.InitFlags(fs)
+
+	if err := fs.Set("logtostderr", "false"); err != nil {
+		return fmt.Errorf("set logtostderr: %w", err)
+	}
+
+	if err := fs.Set("alsologtostderr", "false"); err != nil {
+		return fmt.Errorf("set alsologtostderr: %w", err)
+	}
+
+	if err := fs.Set("stderrthreshold", "5"); err != nil {
+		return fmt.Errorf("set stderrthreshold: %w", err)
+	}
+
+	// Suppress info and warnings from client-go reflector
+	klog.SetOutputBySeverity("INFO", ioutil.Discard)
+	klog.SetOutputBySeverity("WARNING", ioutil.Discard)
+	klog.SetOutputBySeverity("ERROR", ioutil.Discard)
+	klog.SetOutputBySeverity("FATAL", logboek.Context(ctx).ErrStream())
+
+	return nil
+}
+
+func silenceKlogV2(ctx context.Context) error {
+	fs := flag.NewFlagSet("klog", flag.PanicOnError)
+	klog_v2.InitFlags(fs)
+
+	if err := fs.Set("logtostderr", "false"); err != nil {
+		return fmt.Errorf("set logtostderr: %w", err)
+	}
+
+	if err := fs.Set("alsologtostderr", "false"); err != nil {
+		return fmt.Errorf("set alsologtostderr: %w", err)
+	}
+
+	if err := fs.Set("stderrthreshold", "5"); err != nil {
+		return fmt.Errorf("set stderrthreshold: %w", err)
+	}
+
+	// Suppress info and warnings from client-go reflector
+	klog_v2.SetOutputBySeverity("INFO", ioutil.Discard)
+	klog_v2.SetOutputBySeverity("WARNING", ioutil.Discard)
+	klog_v2.SetOutputBySeverity("ERROR", ioutil.Discard)
+	klog_v2.SetOutputBySeverity("FATAL", logboek.Context(ctx).ErrStream())
+
+	return nil
+}
