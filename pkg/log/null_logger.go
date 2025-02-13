2670029de889dac71413f0cf080604dd2e2402f4
Ilya Lesikov <ilya@lesikov.com>
2025-02-13 12:41:54 +0300

    refactor: add *Push/Pop methods to logger
    Signed-off-by: Ilya Lesikov <ilya@lesikov.com>

--- pkg/log/null_logger.go
+++ pkg/log/null_logger.go
@@ -19,13 +19,33 @@ func (l *NullLogger) Trace(ctx context.Context, format string, a ...interface{})
 func (l *NullLogger) TraceStruct(ctx context.Context, obj interface{}, format string, a ...interface{}) {
 }
 
+func (l *NullLogger) TracePush(ctx context.Context, group string, format string, a ...interface{}) {}
+
+func (l *NullLogger) TracePop(ctx context.Context, group string) {}
+
 func (l *NullLogger) Debug(ctx context.Context, format string, a ...interface{}) {}
+
+func (l *NullLogger) DebugPush(ctx context.Context, group string, format string, a ...interface{}) {}
+
+func (l *NullLogger) DebugPop(ctx context.Context, group string) {}
 
 func (l *NullLogger) Info(ctx context.Context, format string, a ...interface{}) {}
 
+func (l *NullLogger) InfoPush(ctx context.Context, group string, format string, a ...interface{}) {}
+
+func (l *NullLogger) InfoPop(ctx context.Context, group string) {}
+
 func (l *NullLogger) Warn(ctx context.Context, format string, a ...interface{}) {}
 
+func (l *NullLogger) WarnPush(ctx context.Context, group string, format string, a ...interface{}) {}
+
+func (l *NullLogger) WarnPop(ctx context.Context, group string) {}
+
 func (l *NullLogger) Error(ctx context.Context, format string, a ...interface{}) {}
+
+func (l *NullLogger) ErrorPush(ctx context.Context, group string, format string, a ...interface{}) {}
+
+func (l *NullLogger) ErrorPop(ctx context.Context, group string) {}
 
 func (l *NullLogger) InfoBlock(ctx context.Context, format string, a ...interface{}) types.LogBlockInterface {
 	return nil
