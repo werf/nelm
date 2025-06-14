ffa7103ef3d5522aac93c07c7f97d7f2e91e59ce
Vladimir Kuznichenkov <kuzaxak.tech@gmail.com>
2025-06-14 20:36:28 +0300

    Add enhanced sensitive data handling
    Functionality adds similar to [helm-diff][1] style of diffing sensitive
    data.
    
    Previously, NELM handled sensitive data with an all-or-nothing approach:
    - Resources marked as sensitive (via `werf.io/sensitive: "true"`) were
    completely hidden - Secrets were entirely hidden, providing no
    visibility into structural changes - No way to selectively redact only
    sensitive fields while showing the rest
    
    This made it difficult to: - Track changes in Secret keys or metadata -
    See the size of sensitive data changes - Selectively protect only truly
    sensitive fields in complex resources
    
    Added support for `werf.io/sensitive-paths` annotation that accepts a
    JSON array of JSONPath expressions:
    
    ```yaml metadata: annotations: werf.io/sensitive-paths:
    '["data.password", "spec.template.spec.containers.*.env.*.value"]' ```
    
    Sensitive values are replaced with size information: ``` password:
    REDACTED (len 12 bytes) ```
    
    [1]: https://github.com/databus23/helm-diff
    
    Signed-off-by: Vladimir Kuznichenkov <kuzaxak.tech@gmail.com>

--- internal/resource/util.go
+++ internal/resource/util.go
@@ -1,6 +1,8 @@
 package resource
 
 import (
+	"encoding/json"
+	"fmt"
 	"regexp"
 	"strconv"
 	"strings"
@@ -10,20 +12,40 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
+const (
+	HideAll = "$$HIDE_ALL$$"
+)
+
+type SensitiveInfo struct {
+	IsSensitive    bool
+	SensitivePaths []string
+}
+
 func IsSensitive(groupKind schema.GroupKind, annotations map[string]string) bool {
+	info := GetSensitiveInfo(groupKind, annotations)
+	return info.IsSensitive
+}
+
+func GetSensitiveInfo(groupKind schema.GroupKind, annotations map[string]string) SensitiveInfo {
 	if _, value, found := FindAnnotationOrLabelByKeyPattern(annotations, annotationKeyPatternSensitive); found {
 		sensitive := lo.Must(strconv.ParseBool(value))
-
 		if sensitive {
-			return true
+			return SensitiveInfo{IsSensitive: true, SensitivePaths: []string{HideAll}}
+		}
+	}
+
+	if _, value, found := FindAnnotationOrLabelByKeyPattern(annotations, annotationKeyPatternSensitivePaths); found {
+		var paths []string
+		if err := json.Unmarshal([]byte(value), &paths); err == nil && len(paths) > 0 {
+			return SensitiveInfo{IsSensitive: true, SensitivePaths: paths}
 		}
 	}
 
 	if groupKind == (schema.GroupKind{Group: "", Kind: "Secret"}) {
-		return true
+		return SensitiveInfo{IsSensitive: true, SensitivePaths: []string{"data.*"}}
 	}
 
-	return false
+	return SensitiveInfo{IsSensitive: false, SensitivePaths: nil}
 }
 
 func IsHook(annotations map[string]string) bool {
@@ -52,6 +74,127 @@ func FindAnnotationsOrLabelsByKeyPattern(annotationsOrLabels map[string]string, 
 	}
 
 	return result, len(result) > 0
+}
+
+func RedactSensitiveData(unstruct *unstructured.Unstructured, sensitivePaths []string) *unstructured.Unstructured {
+	if len(sensitivePaths) == 0 {
+		return unstruct
+	}
+
+	copy := unstruct.DeepCopy()
+
+	for _, path := range sensitivePaths {
+		if path == HideAll {
+			return &unstructured.Unstructured{Object: map[string]interface{}{
+				"apiVersion": copy.GetAPIVersion(),
+				"kind":       copy.GetKind(),
+				"metadata": map[string]interface{}{
+					"name":      copy.GetName(),
+					"namespace": copy.GetNamespace(),
+				},
+			}}
+		}
+
+		redactAtPath(copy.Object, strings.Split(path, "."))
+	}
+
+	return copy
+}
+
+func redactAtPath(obj map[string]interface{}, pathParts []string) {
+	if len(pathParts) == 0 {
+		return
+	}
+
+	key := pathParts[0]
+	if len(pathParts) == 1 {
+		if key == "*" {
+			for k, v := range obj {
+				if val, ok := v.(string); ok {
+					obj[k] = fmt.Sprintf("REDACTED (len %d bytes)", len(val))
+				} else if val, ok := v.([]byte); ok {
+					obj[k] = fmt.Sprintf("REDACTED (len %d bytes)", len(val))
+				} else {
+					obj[k] = "REDACTED"
+				}
+			}
+		} else if val, exists := obj[key]; exists {
+			if strVal, ok := val.(string); ok {
+				obj[key] = fmt.Sprintf("REDACTED (len %d bytes)", len(strVal))
+			} else if byteVal, ok := val.([]byte); ok {
+				obj[key] = fmt.Sprintf("REDACTED (len %d bytes)", len(byteVal))
+			} else {
+				obj[key] = "REDACTED"
+			}
+		}
+		return
+	}
+
+	if key == "*" {
+		for _, v := range obj {
+			if nestedObj, ok := v.(map[string]interface{}); ok {
+				redactAtPath(nestedObj, pathParts[1:])
+			}
+		}
+	} else if val, exists := obj[key]; exists {
+		if nestedObj, ok := val.(map[string]interface{}); ok {
+			redactAtPath(nestedObj, pathParts[1:])
+		} else if slice, ok := val.([]interface{}); ok {
+			redactAtPathInSlice(slice, pathParts[1:])
+		}
+	}
+}
+
+func redactAtPathInSlice(slice []interface{}, pathParts []string) {
+	if len(pathParts) == 0 {
+		return
+	}
+
+	key := pathParts[0]
+
+	// Handle numeric indices
+	if idx, err := strconv.Atoi(key); err == nil {
+		if idx >= 0 && idx < len(slice) {
+			if len(pathParts) == 1 {
+				// Redact the element at this index
+				if val, ok := slice[idx].(string); ok {
+					slice[idx] = fmt.Sprintf("REDACTED (len %d bytes)", len(val))
+				} else if val, ok := slice[idx].([]byte); ok {
+					slice[idx] = fmt.Sprintf("REDACTED (len %d bytes)", len(val))
+				} else {
+					slice[idx] = "REDACTED"
+				}
+			} else {
+				// Continue traversing deeper
+				if nestedObj, ok := slice[idx].(map[string]interface{}); ok {
+					redactAtPath(nestedObj, pathParts[1:])
+				} else if nestedSlice, ok := slice[idx].([]interface{}); ok {
+					redactAtPathInSlice(nestedSlice, pathParts[1:])
+				}
+			}
+		}
+	} else if key == "*" {
+		// Handle wildcard for all elements in slice
+		for i := range slice {
+			if len(pathParts) == 1 {
+				// Redact all elements
+				if val, ok := slice[i].(string); ok {
+					slice[i] = fmt.Sprintf("REDACTED (len %d bytes)", len(val))
+				} else if val, ok := slice[i].([]byte); ok {
+					slice[i] = fmt.Sprintf("REDACTED (len %d bytes)", len(val))
+				} else {
+					slice[i] = "REDACTED"
+				}
+			} else {
+				// Continue traversing deeper for all elements
+				if nestedObj, ok := slice[i].(map[string]interface{}); ok {
+					redactAtPath(nestedObj, pathParts[1:])
+				} else if nestedSlice, ok := slice[i].([]interface{}); ok {
+					redactAtPathInSlice(nestedSlice, pathParts[1:])
+				}
+			}
+		}
+	}
 }
 
 func setAnnotationsAndLabels(res *unstructured.Unstructured, annotations, labels map[string]string) {
