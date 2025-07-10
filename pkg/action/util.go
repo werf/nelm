69dc5325dda560e8bf57c953297faee30a4dde85
Ilya Lesikov <ilya@lesikov.com>
2025-07-10 13:50:49 +0300

    fix: hide libraries logs unless log level is debug
    Signed-off-by: Ilya Lesikov <ilya@lesikov.com>

--- pkg/action/util.go
+++ pkg/action/util.go
@@ -17,6 +17,7 @@ import (
 	klogv2 "k8s.io/klog/v2"
 
 	"github.com/werf/3p-helm/pkg/engine"
+	kddebug "github.com/werf/kubedog/pkg/tracker/debug"
 	"github.com/werf/logboek"
 	"github.com/werf/nelm/pkg/log"
 )
@@ -33,76 +34,40 @@ func SetupLogging(ctx context.Context, logLevel string, opts SetupLoggingOptions
 
 	log.Default.SetLevel(ctx, log.Level(logLevel))
 
-	klog.SetOutputBySeverity("FATAL", logboek.Context(ctx).ErrStream())
-	klog.SetOutputBySeverity("ERROR", logboek.Context(ctx).ErrStream())
-
-	klogv2.SetOutputBySeverity("FATAL", logboek.Context(ctx).ErrStream())
-	klogv2.SetOutputBySeverity("ERROR", logboek.Context(ctx).ErrStream())
-
 	spew.Config.DisablePointerAddresses = true
 	spew.Config.DisableCapacities = true
 
 	switch logLevel {
-	case SilentLogLevel:
+	case SilentLogLevel, ErrorLogLevel, WarningLogLevel, InfoLogLevel:
 		stdlog.SetOutput(io.Discard)
 
+		klog.SetOutputBySeverity("FATAL", ioutil.Discard)
+		klog.SetOutputBySeverity("ERROR", ioutil.Discard)
 		klog.SetOutputBySeverity("WARNING", ioutil.Discard)
 		klog.SetOutputBySeverity("INFO", ioutil.Discard)
 
+		klogv2.SetOutputBySeverity("FATAL", ioutil.Discard)
+		klogv2.SetOutputBySeverity("ERROR", ioutil.Discard)
 		klogv2.SetOutputBySeverity("WARNING", ioutil.Discard)
 		klogv2.SetOutputBySeverity("INFO", ioutil.Discard)
 
 		logrus.SetOutput(ioutil.Discard)
 
 		contdlog.L.Logger.SetOutput(ioutil.Discard)
-	case ErrorLogLevel:
-		stdlog.SetOutput(io.Discard)
 
-		klog.SetOutputBySeverity("WARNING", ioutil.Discard)
-		klog.SetOutputBySeverity("INFO", ioutil.Discard)
+		engine.Debug = false
 
-		klogv2.SetOutputBySeverity("WARNING", ioutil.Discard)
-		klogv2.SetOutputBySeverity("INFO", ioutil.Discard)
-
-		logrus.SetOutput(logboek.Context(ctx).ErrStream())
-		logrus.SetLevel(logrus.ErrorLevel)
-
-		contdlog.L.Logger.SetOutput(logboek.Context(ctx).ErrStream())
-		contdlog.L.Logger.SetLevel(logrus.ErrorLevel)
-	case WarningLogLevel:
-		stdlog.SetOutput(io.Discard)
-
-		klog.SetOutputBySeverity("WARNING", logboek.Context(ctx).ErrStream())
-		klog.SetOutputBySeverity("INFO", ioutil.Discard)
-
-		klogv2.SetOutputBySeverity("WARNING", logboek.Context(ctx).ErrStream())
-		klogv2.SetOutputBySeverity("INFO", ioutil.Discard)
-
-		logrus.SetOutput(logboek.Context(ctx).ErrStream())
-		logrus.SetLevel(logrus.WarnLevel)
-
-		contdlog.L.Logger.SetOutput(logboek.Context(ctx).ErrStream())
-		contdlog.L.Logger.SetLevel(logrus.WarnLevel)
-	case InfoLogLevel:
-		stdlog.SetOutput(io.Discard)
-
-		klog.SetOutputBySeverity("WARNING", logboek.Context(ctx).ErrStream())
-		klog.SetOutputBySeverity("INFO", ioutil.Discard)
-
-		klogv2.SetOutputBySeverity("WARNING", logboek.Context(ctx).ErrStream())
-		klogv2.SetOutputBySeverity("INFO", ioutil.Discard)
-
-		logrus.SetOutput(logboek.Context(ctx).ErrStream())
-		logrus.SetLevel(logrus.WarnLevel)
-
-		contdlog.L.Logger.SetOutput(logboek.Context(ctx).ErrStream())
-		contdlog.L.Logger.SetLevel(logrus.WarnLevel)
+		kddebug.SetDebug(false)
 	case DebugLogLevel:
 		stdlog.SetOutput(os.Stdout)
 
+		klog.SetOutputBySeverity("FATAL", logboek.Context(ctx).ErrStream())
+		klog.SetOutputBySeverity("ERROR", logboek.Context(ctx).ErrStream())
 		klog.SetOutputBySeverity("WARNING", logboek.Context(ctx).ErrStream())
 		klog.SetOutputBySeverity("INFO", logboek.Context(ctx).OutStream())
 
+		klogv2.SetOutputBySeverity("FATAL", logboek.Context(ctx).ErrStream())
+		klogv2.SetOutputBySeverity("ERROR", logboek.Context(ctx).ErrStream())
 		klogv2.SetOutputBySeverity("WARNING", logboek.Context(ctx).ErrStream())
 		klogv2.SetOutputBySeverity("INFO", logboek.Context(ctx).OutStream())
 
@@ -113,12 +78,18 @@ func SetupLogging(ctx context.Context, logLevel string, opts SetupLoggingOptions
 		contdlog.L.Logger.SetLevel(logrus.DebugLevel)
 
 		engine.Debug = true
+
+		kddebug.SetDebug(true)
 	case TraceLogLevel:
 		stdlog.SetOutput(os.Stdout)
 
+		klog.SetOutputBySeverity("FATAL", logboek.Context(ctx).ErrStream())
+		klog.SetOutputBySeverity("ERROR", logboek.Context(ctx).ErrStream())
 		klog.SetOutputBySeverity("WARNING", logboek.Context(ctx).ErrStream())
 		klog.SetOutputBySeverity("INFO", logboek.Context(ctx).OutStream())
 
+		klogv2.SetOutputBySeverity("FATAL", logboek.Context(ctx).ErrStream())
+		klogv2.SetOutputBySeverity("ERROR", logboek.Context(ctx).ErrStream())
 		klogv2.SetOutputBySeverity("WARNING", logboek.Context(ctx).ErrStream())
 		klogv2.SetOutputBySeverity("INFO", logboek.Context(ctx).OutStream())
 
@@ -129,6 +100,8 @@ func SetupLogging(ctx context.Context, logLevel string, opts SetupLoggingOptions
 		contdlog.L.Logger.SetLevel(logrus.TraceLevel)
 
 		engine.Debug = true
+
+		kddebug.SetDebug(true)
 	default:
 		panic(fmt.Sprintf("unknown log level %q", logLevel))
 	}
