e6a68ae8be181a12e0100b0e36093ea07527d07d
Dmitry Bochkarev <dimabochkarev@gmail.com>
2026-02-01 04:08:17 +0100

    feat: werf giterminism for typescript charts
    Signed-off-by: Dmitry Bochkarev <dimabochkarev@gmail.com>

--- internal/ts/files.go
+++ internal/ts/files.go
@@ -1,12 +1,10 @@
 package ts
 
 import (
-	"fmt"
-	"os"
-	"path/filepath"
 	"strings"
 
 	helmchart "github.com/werf/3p-helm/pkg/chart"
+	"github.com/werf/3p-helm/pkg/werf/file"
 	"github.com/werf/nelm/pkg/common"
 )
 
@@ -21,23 +19,6 @@ func extractSourceFiles(files []*helmchart.File) map[string][]byte {
 	return sourceFiles
 }
 
-func findEntrypointInDir(tsDir string) (string, error) {
-	for _, ep := range common.ChartTSEntryPoints {
-		epPath := filepath.Join(tsDir, ep)
-
-		_, err := os.Stat(epPath)
-		if err == nil {
-			return ep, nil
-		}
-
-		if !os.IsNotExist(err) {
-			return "", fmt.Errorf("stat %s: %w", epPath, err)
-		}
-	}
-
-	return "", nil
-}
-
 func findEntrypointInFiles(files map[string][]byte) string {
 	for _, ep := range common.ChartTSEntryPoints {
 		if _, ok := files[ep]; ok {
@@ -46,4 +27,25 @@ func findEntrypointInFiles(files map[string][]byte) string {
 	}
 
 	return ""
+}
+
+func filterTSFiles(files []*file.ChartExtenderBufferedFile) map[string][]byte {
+	result := make(map[string][]byte)
+	for _, f := range files {
+		if strings.HasPrefix(f.Name, common.ChartTSSourceDir) {
+			result[strings.TrimPrefix(f.Name, common.ChartTSSourceDir)] = f.Data
+		}
+	}
+
+	return result
+}
+
+func hasNodeModules(files map[string][]byte) bool {
+	for name := range files {
+		if strings.HasPrefix(name, "node_modules/") {
+			return true
+		}
+	}
+
+	return false
 }
