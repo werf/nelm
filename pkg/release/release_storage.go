a52a7004c722e5be76fdc83adf44ce68b8bbba94
Ilya Lesikov <ilya@lesikov.com>
2026-07-23 12:03:50 +0300

    fix: optimize `release get`
    Signed-off-by: Ilya Lesikov <ilya@lesikov.com>

--- pkg/release/release_storage.go
+++ pkg/release/release_storage.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/metadata"
 
 	helmaction "github.com/werf/nelm/pkg/helm/pkg/action"
 	helmrelease "github.com/werf/nelm/pkg/helm/pkg/release"
@@ -22,6 +23,8 @@ type ReleaseStorager interface {
 	Update(rls *helmrelease.Release) error
 	Delete(name string, version int) (*helmrelease.Release, error)
 	Query(labels map[string]string) ([]*helmrelease.Release, error)
+	// GetRelease returns a single release revision. version == 0 means the latest revision.
+	GetRelease(name string, version int) (*helmrelease.Release, error)
 }
 
 type ReleaseStorageOptions struct {
@@ -31,8 +34,6 @@ type ReleaseStorageOptions struct {
 
 // Constructs Helm release storage driver.
 func NewReleaseStorage(ctx context.Context, namespace, storageDriver string, clientFactory kube.ClientFactorier, opts ReleaseStorageOptions) (*helmstorage.Storage, error) {
-	var storage *helmstorage.Storage
-
 	lazyClient := helmaction.NewLazyClient(namespace, func() (*kubernetes.Clientset, error) {
 		return clientFactory.Static().(*kubernetes.Clientset), nil
 	})
@@ -41,15 +42,31 @@ func NewReleaseStorage(ctx context.Context, namespace, storageDriver string, cli
 		log.Default.Debug(ctx, format, a...)
 	}
 
+	var storage *helmstorage.Storage
+
 	switch storageDriver {
 	case "secret", "secrets", "":
+		metadataClient, err := metadata.NewForConfig(clientFactory.KubeConfig().RestConfig)
+		if err != nil {
+			return nil, fmt.Errorf("construct release metadata client: %w", err)
+		}
+
 		driver := helmdriver.NewSecrets(helmaction.NewSecretClient(lazyClient))
 		driver.Log = logFn
+		driver.MetadataClient = metadataClient
+		driver.Namespace = namespace
 
 		storage = helmstorage.Init(driver)
 	case "configmap", "configmaps":
+		metadataClient, err := metadata.NewForConfig(clientFactory.KubeConfig().RestConfig)
+		if err != nil {
+			return nil, fmt.Errorf("construct release metadata client: %w", err)
+		}
+
 		driver := helmdriver.NewConfigMaps(helmaction.NewConfigMapClient(lazyClient))
 		driver.Log = logFn
+		driver.MetadataClient = metadataClient
+		driver.Namespace = namespace
 
 		storage = helmstorage.Init(driver)
 	case "memory":
